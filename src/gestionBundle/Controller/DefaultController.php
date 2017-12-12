81ed02d978582f92798ac1079858737916e4d5a5
salih gungor <lucievanbaelinghem@hotmail.fr>
2017-12-12 16:21:09 +0100

    date

--- src/gestionBundle/Controller/DefaultController.php
+++ src/gestionBundle/Controller/DefaultController.php
@@ -30,9 +30,11 @@ class DefaultController extends Controller
 		$formBuilder->add('nom','text',array('label'=>'Saisir le nom : '));
 		$formBuilder->add('prenom','text',array('label'=>'Saisir le prenom : '));
 		$formBuilder->add('codepostal','number',array('label'=>'Saisir votre code postal : '));
-		$formBuilder->add('datenaiss','date',array('label'=>'Saisir votre date de naissance : '));
+		$formBuilder->add('datenaiss','date',array('label'=>'Saisir votre date de naissance : ','placeholder' => array(
+        'year' => 'Annee', 'month' => 'Mois', 'day' => 'Jour'
+    )));
 		$formBuilder->add('mail','text',array('label'=>'Saisir votre mail : '));
-		$formBuilder->add('assurer','number',array('label'=>'Saisir vi vous ete assurer ou ayan-droit : '));
+		$formBuilder->add('assurer',CheckboxType::class,array('label'=>'Saisir vi vous ete assurer ou ayan-droit : ','required' => false));
 		$formBuilder->add('save','submit');
 
 
@@ -76,7 +78,9 @@ class DefaultController extends Controller
         $formBuilder->add('nom','text',array('label'=>'Saisir le nom : '));
         $formBuilder->add('prenom','text',array('label'=>'Saisir le prenom : '));
         $formBuilder->add('codepostal','number',array('label'=>'Saisir votre code postal : '));
-        $formBuilder->add('datenaiss','date',array('label'=>'Saisir votre date de naissance : '));
+        $formBuilder->add('datenaiss','date',array('label'=>'Saisir votre date de naissance : ','placeholder' => array(
+        'year' => 'Annee', 'month' => 'Mois', 'day' => 'Jour'
+    )));
         $formBuilder->add('mail','text',array('label'=>'Saisir votre mail : '));
         $formBuilder->add('assurer',CheckboxType::class,array('label'=>'Saisir vi vous ete assurer ou ayan-droit : ','required' => false));
         $formBuilder->add('save','submit');
